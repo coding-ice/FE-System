b11100d9ba85bcf39e31c87c1521fbd96fdfc251
ice <1597834867@qq.com>
2024-09-18 22:58:33 +0800

    feat: 事件委托

---
+++ docs/js/dom-event/demo/Delegation.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="wrap" @click="handleClick">
+    <template v-for="val in list" :key="val">
+      <li :data-value="val" :class="{ [$style.grey]: selected === val }">{{ val }}</li>
+    </template>
+  </ul>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const selected = ref('1')
+
+const list = ['1', '2', '3', '4', '5', '6']
+
+const handleClick = (e) => {
+  // 事件发生的元素
+  const target = e.target
+  if (target.tagName !== 'LI') return
+
+  const value = target.dataset.value
+  selected.value = value
+}
+
+
+</script>
+<style module>
+.grey {
+  background: #282a2f;
+}
+
+li {
+  cursor: pointer;
+}
+</style>
