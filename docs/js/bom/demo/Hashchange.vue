0c8822715b8bc0cabbd90ceef47e6031f92e0ca0
ice <1597834867@qq.com>
2024-09-19 22:38:03 +0800

    feat: hash change

--- docs/js/bom/demo/Hashchange.vue
+++ docs/js/bom/demo/Hashchange.vue
@@ -1,9 +1,24 @@
+<template>
+  <button @click="handleClick">change hash</button>
+</template>
 <script setup>
 import { onMounted } from 'vue'
 
+const handleClick = () => {
+  location.hash = 'ice'
+}
+
 onMounted(() => {
   window.addEventListener('hashchange', function (e) {
-    console.log(`newURL: ${e.newURL} oldURL: ${e.oldURL}`)
+    console.log('hash ->', decodeURIComponent(location.hash))
   })
 })
 </script>
+
+<style scoped>
+button {
+  background: var(--vp-custom-block-details-bg);
+  padding: 5px;
+  border-radius: 5px;
+}
+</style>
