ded0b4ae825f939941c4dfce2445afa8c7363832
ice <1999iceweb@gmail.com>
2025-11-30 14:15:16 +0800

    docs: update ICEPromise implementation and remove outdated files

--- docs/write/async/promise-a/code/1.promise-v1.js
+++ docs/write/async/promise-a/code/1.promise-v1.js
@@ -1,76 +1,72 @@
 /**
- * 1. 初始化状态
- * 2. 实现 resolve 和 reject
- * 3. 实现了 then 方法 （支持同步异步， then 方法的多次调用）
+ * 1. 构造器接受一个 executor
+ * 2. 定义变量 status / value / reason / onFulfilledCbs / onRejectedCbs
+ * 3. 向 executor 传递 resolve，reject（要箭头函数，不然会有 this 指向的问题）
+ * 4. resolve reject 分别接受 value / reason （异步执行， onFulfilledCbs / onRejectedCbs）
+ * 5. 当执行 then 函数，分别接受 成功/失败的 cb，往队列中 push，然后分别执行
  */
-class IcePromise {
-  // 1. promise 的状态
-  static PENDING = "pending";
-  static FULFILLED = "fulfilled"; 
-  static REJECTED = "rejected";
-
+class ICEPromise {
   constructor(executor) {
-    // 2. 初始化值
-    this.status = IcePromise.PENDING;
-    this.value = null;
-    this.reason = null;
-    this.onFulfilledCallbacks = [];
-    this.onRejectedCallbacks = [];
+    this.status = "PENDING";
+    this.value = undefined;
+    this.reason = undefined;
+    this.onFulfilledCbs = [];
+    this.onRejectedCbs = [];
 
-    // 3. 实现 resolve
     const resolve = (value) => {
-      if (this.status === IcePromise.PENDING) {
-        this.status = IcePromise.FULFILLED;
+      if (this.status === "PENDING") {
+        this.status = "FULFILLED";
         this.value = value;
 
-        this.onFulfilledCallbacks.forEach((cb) => cb(value));
+        setTimeout(() => {
+          this.onFulfilledCbs.forEach((fn) => fn(value));
+        });
       }
     };
 
-    // 4. 实现 reject
     const reject = (reason) => {
-      if (this.status === IcePromise.PENDING) {
-        this.status = IcePromise.REJECTED;
+      if (this.status === "PENDING") {
+        this.status = "REJECTED";
         this.reason = reason;
 
-        this.onRejectedCallbacks.forEach((cb) => cb(reason));
+        setTimeout(() => {
+          this.onRejectedCbs.forEach((fn) => fn(reason));
+        });
       }
     };
 
     try {
-      // 5. 立即执行执行 executor
       executor(resolve, reject);
-    } catch (error) {
-      // 6. 如果执行 executor 时抛出异常，则将异常作为 reject 的参数
-      reject(error);
+    } catch (e) {
+      reject(e);
     }
   }
 
   then(onFulfilled, onRejected) {
-    // 7. 如果 onFulfilled 不是函数，则将其转换为函数
-    onFulfilled =
-      typeof onFulfilled === "function" ? onFulfilled : (value) => value;
-    // 8. 如果 onRejected 不是函数，则将其转换为函数
-    onRejected =
-      typeof onRejected === "function"
-        ? onRejected
-        : (reason) => {
-            throw reason;
-          };
-
-    // 10. fulfilled 或者 rejected 状态 -> 其实是同步的 （因为在调用 then 的时候，promise 的状态已经确定了）
-    if (this.status === IcePromise.FULFILLED) {
-      setTimeout(() => onFulfilled(this.value));
-    } else if (this.status === IcePromise.REJECTED) {
-      setTimeout(() => onRejected(this.reason));
-    } else {
-      // 11. pending 状态 -> 异步的
-      this.onFulfilledCallbacks.push(() => {
-        setTimeout(() => onFulfilled(this.value));
-      });
-      this.onRejectedCallbacks.push(() => {
-        setTimeout(() => onRejected(this.reason));
-      });
-    }
+    this.onFulfilledCbs.push(onFulfilled);
+    this.onRejectedCbs.push(onRejected);
   }
 }
+
+const p = new ICEPromise((resolve, reject) => {
+  // resolve(1);
+  reject(2);
+});
+
+p.then(
+  (value) => {
+    console.log("res1: ", value);
+  },
+  (err) => {
+    console.log("err1:", err);
+  }
+);
+
+p.then(
+  (value) => {
+    console.log("res2: ", value);
+  },
+  (err) => {
+    console.log("err2:", err);
+  }
+);
